f8671f9840cf90129c8a61402025398b5a82cca6
lumskii <lumi8866@gmail.com>
2022-09-02 10:38:03 -0700

    first commit

---
+++ clients-portal/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCcPJHR67MtN-kyWWRU0vugvQqG9HhCcqU",
+    authDomain: "clients-portal-omm.firebaseapp.com",
+    projectId: "clients-portal-omm",
+    storageBucket: "clients-portal-omm.appspot.com",
+    messagingSenderId: "841911928899",
+    appId: "1:841911928899:web:e374c71454f380eb5d6cf6",
+    measurementId: "G-7XMC1D007S"
+  };
