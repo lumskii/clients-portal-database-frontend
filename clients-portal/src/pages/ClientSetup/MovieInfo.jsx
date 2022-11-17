ae3ce31479f39f41e4759f0a3719df5d384cf254
lumskii <lumi8866@gmail.com>
2022-11-16 22:02:05 -0700

    home pc edit

--- clients-portal/src/pages/ClientSetup/MovieInfo.jsx
+++ clients-portal/src/pages/ClientSetup/MovieInfo.jsx
@@ -1,56 +1,59 @@
 import React from 'react'
-import { Box, Button, TextField } from "@mui/material";
-import { Formik } from "formik";
+import { TextField } from "@mui/material";
+
 
 export default function MovieInfo({details, handleChange, checkoutSchema, initialState}) {
 
   return (
-          <>
-                <TextField
-                  fullWidth
-                  variant="filled"
-                  type="text"
-                  label="Film Name"
-                  onChange={handleChange}
-                  value={details.filmName}
-                  name="filmName"
-                  errorMesssage="This Should not include any special characters"
-                  // error={!!touched.filmName && !!errors.filmName}
-                  // helperText={touched.filmName && errors.filmName}
-                  sx={{ gridColumn: "span 2" }} 
-                />
-                <p>Producer's Email</p>
-                <input
-                  className="text_area"
-                  type="email"
-                  name="producersEmail"
-                  value={details.producersEmail}
-                  onChange={handleChange}
-                />
-                <p>Film's Code</p>
-                <input
-                  className="text_area"
-                  type="text"
-                  value={details.filmsCode}
-                  name="filmsCode"
-                />
-                <p>Distribution type</p>
-                <select
-                  id="dropdown"
-                  className="text_area"
-                  name="distributionType"
-                  value={details.distributionType}
-                  onChange={handleChange}
-                >
-                  <option disabled selected value="">
-                    Please select category
-                  </option>
-                  <option value="sales">Sales Only</option>
-                  <option value="distribution">Distribution Only</option>
-                  <option value="sales_distribution">Sales & Distribution</option>
-                </select>
-          </>
-          
-          
-  )
+    <>
+      <TextField
+        fullWidth
+        variant="filled"
+        type="text"
+        label="Film Name"
+        onChange={handleChange}
+        value={details.filmName}
+        name="filmName"
+        errorMesssage="This Should not include any special characters"
+        // error={!!touched.filmName && !!errors.filmName}
+        // helperText={touched.filmName && errors.filmName}
+        sx={{ gridColumn: "span 2" }}
+      />
+
+      <TextField
+        fullWidth
+        variant="filled"
+        label="Producer's Email"
+        type="email"
+        name="producersEmail"
+        value={details.producersEmail}
+        onChange={handleChange}
+        sx={{ gridColumn: "span 2" }}
+      />
+      <TextField
+        fullWidth
+        variant="filled"
+        label="Film's Code"
+        type="text"
+        value={details.filmsCode}
+        name="filmsCode"
+        sx={{ gridColumn: "span 2" }}
+      />
+      <p>Distribution type</p>
+      <select
+        id="dropdown"
+        className="text_area"
+        name="distributionType"
+        value={details.distributionType}
+        onChange={handleChange}
+      >
+        <option disabled selected value="">
+          Please select category
+        </option>
+        <option value="sales">Sales Only</option>
+        <option value="distribution">Distribution Only</option>
+        <option value="sales_distribution">Sales & Distribution</option>
+      </select>
+    </>
+  );
 }
